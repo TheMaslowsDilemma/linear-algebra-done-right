f9116668398e20e0fa7265e705f6f17363fa6d69
themaslowsdilemma <themaslowsdilemma@gmail.com>
2025-12-02 07:42:48 -0500

    format

--- LADR/bin/main.ml
+++ LADR/bin/main.ml
@@ -1,11 +1,11 @@
 open LADR
 
-
 (*** Excercise 1A ***)
 let p7 =
-	let open Complex in
-	let u = Complex.of_pair (-1. /. 2., (sqrt 3.0) /. 2.) in (* (-1 + sqrt 3) / 2 ) *)
-	let v = Complex.mul (Complex.mul u u) u in
-	Complex.to_string v
+  let open Complex in
+  let u = Complex.of_pair (-1. /. 2., sqrt 3.0 /. 2.) in
+  (* (-1 + sqrt 3) / 2 ) *)
+  let v = Complex.mul (Complex.mul u u) u in
+  Complex.to_string v
 
-let () = Printf.printf "v = %s\n" p7;
+let () = Printf.printf "v = %s\n" p7
--- LADR/lib/complex.mli
+++ LADR/lib/complex.mli
@@ -1,15 +1,12 @@
 module Complex : sig
-	type t
+  type t
 
-	val zero : t
-	val one : t
-	val i : t
-
-	val add	: t -> t -> t
-	val mul	: t -> t -> t
-	val adi	: t -> t
-
-	val of_pair : float * float -> t
-
-	val to_string : t -> string
+  val zero : t
+  val one : t
+  val i : t
+  val add : t -> t -> t
+  val mul : t -> t -> t
+  val adi : t -> t
+  val of_pair : float * float -> t
+  val to_string : t -> string
 end
