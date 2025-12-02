741430d217dc085ac849129ea4aa01b0a34e3fc1
Thomas Flatley <thomasflatley@Thomass-MacBook-Pro.local>
2025-12-02 07:40:32 -0500

    excercise 1A: P7

--- LADR/bin/main.ml
+++ LADR/bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+open LADR
+
+
+(*** Excercise 1A ***)
+let p7 =
+	let open Complex in
+	let u = Complex.of_pair (-1. /. 2., (sqrt 3.0) /. 2.) in (* (-1 + sqrt 3) / 2 ) *)
+	let v = Complex.mul (Complex.mul u u) u in
+	Complex.to_string v
+
+let () = Printf.printf "v = %s\n" p7;
---
+++ LADR/lib/complex.ml
@@ -0,0 +1,15 @@
+module Complex = struct 
+	type t = float * float
+
+	let zero	= (0.0, 0.0)
+	let one		= (1.0, 0.0)
+	let i		= (0.0, 1.0) (* not identity, just sqrt -1 *)
+
+	let add (a, b) (c, d) = (a +. c, b +. d)
+	let mul (a, b) (c, d) = (a *. c -. b *. d, a *. d +. b *. c)
+	let adi (a, b) = (-.a, -.b) (* additive invers *)
+
+	let of_pair (a, b) = (a, b)
+	(* .10g is 10 digits after decimal -- could be higher *)
+	let to_string (a, b) = Printf.sprintf "(%.10g + %.10gi)" a b
+end
