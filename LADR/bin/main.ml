966f9cf088fd9dcf8891e2e0cefa803d24ca28b3
themaslowsdilemma <themaslowsdilemma@gmail.com>
2025-12-09 17:24:42 -0500

    unrelated graphics

--- LADR/bin/main.ml
+++ LADR/bin/main.ml
@@ -1,13 +1,4 @@
 open LADR
-open Graphics
 
 let () =
-  open_graph "";
-  set_window_title "Linear Algebra Done Right";
-  synchronize ();
-  set_color white;
-  clear_graph ();
-  set_color black;
-  let _ = Chapter2.run () in
-  ignore (read_key ());
-  close_graph ();
+  Chapter2.run ()
--- LADR/lib/chapter2.ml
+++ LADR/lib/chapter2.ml
@@ -1,24 +1,46 @@
+(*** Excercises and Experiments with Graphics ***)
+
 (* let exercise_2A_P8 () =
   let open Vector.RealVector in
   let a = of_list [ 3.; 1.; 4. ] in
   let b = of_list [ 2.; -3.; 5. ] in
-  Printf.printf "a + b: %s\n" (add a b |> to_string) *)
+  Printf.printf "a + b: %s\n" (add a b |> to_string)
+*)
 
 let draw_complex_point c =
-  Graphics.set_color Graphics.blue;
-  Graphics.fill_circle (Complex.real c |> int_of_float) (Complex.img c |> int_of_float) 10
+  Graphics.fill_circle (Complex.real c |> int_of_float) (Complex.img c |> int_of_float) 4
 
-let exercise_2A_P8 () =
+
+let three_point_render a =
   let open Vector.ComplexVector in
-  let a = Complex.of_pair (5., -1.5) in
-  let b =
+  let v =
     of_list
       [
-        Complex.of_pair (100., 100.);
-        Complex.of_pair (50., 20.);
-        Complex.of_pair (40., 100.);
+        Complex.of_pair (0.1, 0.1);
+        Complex.of_pair (0.2, 0.2);
+        Complex.of_pair (0.05, 0.05);
       ]
   in
-  Array.map draw_complex_point (scale a b)
+  Array.iter draw_complex_point (scale a v)
 
-let run () = exercise_2A_P8 ()
+let rec render_loop (x, y) =
+  let open Graphics in
+  set_color white; clear_graph ();
+  set_color blue;
+  three_point_render (Complex.of_pair (float_of_int x, float_of_int y));
+  synchronize ();
+  Unix.sleepf 0.016;
+  if button_down () then raise Exit;
+  render_loop (mouse_pos ())
+
+let run () =
+  let open Graphics in
+  open_graph "";
+  set_window_title "Linear Algebra Done Right";
+  auto_synchronize false;
+  try
+    render_loop (Graphics.mouse_pos ())
+  with
+  | Exit ->
+    Graphics.close_graph ();
+    Printf.printf "Exiting\n"
